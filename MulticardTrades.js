da1c943e6dd4262b0e75a7f0231521aeb2f3263b
foilking <foilking21@gmail.com>
2015-03-30 12:28:35 -0500

    Exclude blank rows, shows points for user

--- MulticardTrades.js
+++ MulticardTrades.js
@@ -1,21 +1,28 @@
 var userRows = {};
-$("table.cards-show tbody tr").each(function() {
+var $rows = $("table.cards-show tbody tr:visible");
+$rows.each(function() {
 	var userId = $(this).find(".member a:nth-child(3)").attr("href").substring(15);
 	var rows = userRows[userId];
 	if (userRows[userId] === undefined) { 
-		userRows[userId] = []; 
+		userRows[userId] = {};
+		userRows[userId]["points"] = 0;
+		userRows[userId]["requested"] = 0;
+		userRows[userId]["rows"] = [];
 	}
-	userRows[userId].push($(this).attr("id"));
+	userRows[userId].rows.push($(this).attr("id"));
+	userRows[userId].points = parseInt($(this).find(".points").text(), 10);
+	userRows[userId].requested += parseInt($(this).find(".value").text(), 10);
 });
-$("table.cards-show tbody tr").hide();
+$rows.hide();
 for (var key in userRows) {
-	console.log("Key: " + key);
 	if (userRows.hasOwnProperty(key)){
-		if(userRows[key].length > 1) {
-			for (var row in userRows[key]) {
-				console.log(row);
-				$("#" + userRows[key][row]).show();
-			}
+		if(userRows[key].rows.length > 1) {
+			console.log("Id: " + key + " Points: " + userRows[key].points + " Requested: " + userRows[key].requested);
+			var keyRows = userRows[key].rows;
+			keyRows.forEach(function (element) {
+				$("#" + element).show();
+				$("#" + element).attr("data-user", key);
+			});
 		}
 	}
 }
